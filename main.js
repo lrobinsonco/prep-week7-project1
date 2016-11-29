fa9ad1a628a98de2f544c80da59d4721a730354f
Tim Pender <mrtimpender@gmail.com>
2016-11-28 20:19:08 -0700

    added files

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
+  
   $('#submitBtn').on('click', function(){
     $('#searchResults').empty();
 
     let searchText = $('#searchBox').val();
-    let $query = $.getJSON('http://www.omdbapi.com/?s=' + searchText)
+    let $query = $.getJSON('http://www.omdbapi.com/?s=' + searchText);
 
     $query.done((data) => {
       if ($query.status !== 200) {
           return;
       }
-
+      console.log(data);
       let results = data.Search;
+
+      // Function to take JSON object and populate the HTML DOM
       breakDownSearchResults(results)
 
       $query.fail(function(err) {
@@ -22,19 +25,34 @@ $(document).ready(function() {
 
 // Iterate over results; create container Div; append to DOM
   const breakDownSearchResults = function(array) {
+
+    // FOR LOOP example of populating the DOM with JSON elements
+
+    // for (var i = 0; i < array.length; i++) {
+    //   console.log(array[i]);
+    //   let newResult = document.createElement( 'div' )
+    //   $(newResult).addClass('result')
+    //
+    //   $('#searchResults').append("<h6>"+ array[i]["Title"] +"</h6>");
+    //   $('#searchResults').append("<h6>"+ array[i]["Year"] +"</h6>");
+    //   $('#searchResults').append("<img src='"+ array[i]["Poster"] + " ' />");
+    // }
+
+    // Higher Order Function that Creates each result container Div and Appends it to the DOM
+    // Then calls populateResultDivs function to populate the individual result divs
     array.forEach( (result) => {
       let newResult = document.createElement( "div" )
-      $(newResult).addClass('result')
-      $('#searchResults').append(populateResultDivs(result, newResult))
+      $(newResult).addClass("result")
+      $("#searchResults").append( populateResultDivs(result, newResult))
       console.log(result);
     })
   }
 
 // Populate result div with result specific info
-  const populateResultDivs = function (obj, containerDiv) {
-    let title = obj.Title;
-    let poster = obj.Poster;
-    let releaseYear = obj.Year;
+  const populateResultDivs = function (obj, containerDiv) { // obj = data.Search[0].Title
+    let title = obj.Title;      //Batman Begins
+    let poster = obj.Poster;    //....jpg
+    let releaseYear = obj.Year; // 2005
 
     $(containerDiv).append("<h5>" + title + "</h5>");
     $(containerDiv).append("<h6>" + releaseYear + "</h6>");
